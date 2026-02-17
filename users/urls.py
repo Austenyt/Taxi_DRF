11ba2e8ea2cd531d588a1be3daa514d33f9a88bb
Austenyt <austenyt@gmail.com>
2026-02-17 15:37:15 +0300

    userlist

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from users.views import LoginAPIView, RegisterAPIView
+from users.views import LoginAPIView, RegisterAPIView, UserListView
 
 urlpatterns = [
     path('login/', LoginAPIView.as_view()),
     path('register/', RegisterAPIView.as_view()),
+    path('users/', UserListView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import authenticate
+from django.core.serializers import serialize
 from rest_framework import permissions, status
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from users.serializers import LoginSerializer, RegisterSerializer
+from users.models import User
+from users.serializers import LoginSerializer, RegisterSerializer, UserSerializer
 
 
 class RegisterAPIView(APIView):
@@ -34,3 +36,12 @@ class LoginAPIView(APIView):
             return Response({'token': token.key}, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserListView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
